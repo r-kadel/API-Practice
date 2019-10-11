110c3ad3c196afe8f813aeb6a74d04bc17b09a63
Ryan Kadel <ryan.kadel88@gmail.com>
2019-10-10 21:17:46 -0400

    commit for assignment

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-alert('connected')
+$(watchForm);
+
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        $('.search-results').html('');
+        let searchTerm = $('#user-name-input').val();
+
+        if (searchTerm !== '') {
+            getRepoList(searchTerm);
+        } else {
+            alert('please enter a user name');
+        }
+
+    })
+}
+
+function getRepoList(name) {
+    const params = {
+        type: 'owner',
+        sort: 'created',
+        direction: 'desc'
+    };
+    let userName = $('#user-name-input').val();
+    let searchUrl = `https://api.github.com/users/${userName}/repos`;
+    
+    let queryString = $.param(params);
+    let url = searchUrl + '?' + queryString;
+
+    fetch(url).then(res => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(res.statusText);
+        }
+    })
+        .then(resJson => displayResults(resJson))
+        .catch(err => {
+            $('#error-message').text(`You broke something: ${err.message}`)
+        })
+
+}
+
+function displayResults(resJson) {
+    for (let i = 0; i < resJson.length; i++) {
+        $('.search-results').append(
+            `<li>
+                <h3>${resJson[i].name}</h3>
+                <p>${resJson[i].url}</p>
+            </li>
+            `
+        )
+    }
+    $('.search-results').removeClass('hidden');
+}
